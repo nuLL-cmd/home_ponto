fa1ac025d1dce78606b799ca3cf1412699a55594
null <programadormoderador@hotmail.com>
2022-03-10 23:14:41 -0300

    1 - Update contentView for dataBinding in IntroAppActivity.kt

--- app/src/main/java/com/automatodev/home_ponto/activity/IntroAppActivity.kt
+++ app/src/main/java/com/automatodev/home_ponto/activity/IntroAppActivity.kt
@@ -2,12 +2,17 @@ package com.automatodev.home_ponto.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.automatodev.home_ponto.R
+import com.automatodev.home_ponto.databinding.ActivityIntroAppBinding
 import com.heinrichreimersoftware.materialintro.app.IntroActivity
 
 class IntroAppActivity : IntroActivity() {
+
+    private lateinit var binding:ActivityIntroAppBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro_app)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_intro_app)
     }
 }
