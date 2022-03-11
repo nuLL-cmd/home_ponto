230060e2de9bcda15cf9c716d10b40991d4fa5e0
null <programadormoderador@hotmail.com>
2022-03-10 23:36:35 -0300

    1 - Implementation intent from SplashActivity.kt to IntroAppActivity.kt

--- app/src/main/java/com/automatodev/home_ponto/activity/SplashActivity.kt
+++ app/src/main/java/com/automatodev/home_ponto/activity/SplashActivity.kt
@@ -1,7 +1,9 @@
 package com.automatodev.home_ponto.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.automatodev.home_ponto.R
 import com.automatodev.home_ponto.databinding.ActivitySplashBinding
@@ -13,5 +15,20 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_splash)
+
+        Toast.makeText(this, "Iniciando aplicação.", Toast.LENGTH_SHORT).show()
+        resumeSplashScreen()
+    }
+
+    private fun resumeSplashScreen() {
+        val thread = Thread(){
+            run {
+                Thread.sleep(2000)
+                val intent = Intent(this, IntroAppActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+        thread.start()
     }
 }
